cbd97c6436b2a9cf44bdab990930ee55e4f5d2f7
Acr4n1us <alantopkiller@gmail.com>
2017-08-14 14:58:39 -0300

    Add files via upload

--- subbackdorringmenu.py
+++ subbackdorringmenu.py
@@ -2,7 +2,7 @@ from mainmenu import *
 def subbackdorringmenu():
     from mainmenu import mainmenu
     print ("""
-        1 - EDIT
+        1 - Disable windows 7/8/10 Defender
         2 - EDIT
         3 - EDIT
         0 - Back to frist select menu
@@ -11,7 +11,15 @@ def subbackdorringmenu():
     submenuselect = raw_input(str("Say the option for show: "))
 
     if submenuselect == "1":
-        print ("Edit ok")
+        data= "RamalhoSec"
+        f = open('payloads/Disable-winDefender.ino', 'r')
+        filedata = f.read()
+        f.close()
+        newdata = filedata.replace("CREDS", data)
+        f = open('out/OUT-Disable-winDefender.ino', 'w')
+        f.write(newdata)
+        f.close()
+        os.system("start out/OUT-Disable-winDefender.ino")
 
 
     elif submenuselect == "2":
