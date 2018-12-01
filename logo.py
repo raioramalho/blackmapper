891f8316be539b4f7e7e221248340af5465cc9de
Alan Ramalho <ramalho.sit@gmail.com>
2018-11-30 22:03:13 -0200

    Update logo.py

--- logo.py
+++ logo.py
@@ -14,7 +14,7 @@ def logo1():
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 Scripts for Arduino leonardo Using teeOnArdu lib like a RubberDucky
 Created by Acr4n1us | BRA | 20 JAN 2017 - Edited by R4nd0m
-Demo Version : 1.8 | Updated On 25 AGO 2018-@All Rights Reserved
+Demo Version : 1.9a | Updated On 29 NOV 2018-@All Rights Reserved
 Payloads: x | Original Repo github.com/RamalhoSec/InserKey-tool.git
 Bitcoin = 3DppKRbA9Um3z4wnmVtkqnETnvwsip7WkC
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
