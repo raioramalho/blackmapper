87deb6b81b31c5a0c97882a98e9f1c1bef5fec48
Alan Ramalho <ramalho.sit@gmail.com>
2024-12-25 21:38:59 -0300

    v1.0: qua 25 dez 2024 21:39:45 > Rename all .x files to .ino.

---
+++ tmp/payload.ino
@@ -0,0 +1,54 @@
+const unsigned int ledPin = 17;
+const unsigned int delayTime = 1500;
+void setup()
+{
+  delay(1000);
+
+
+
+  // Do the shit...
+  pwn();
+
+}
+
+// Open an application on Windows via Run
+void openapp(String app)
+{
+  // Windows Key + R to open Run
+  key(KEY_R , MODIFIERKEY_RIGHT_GUI);
+  delay(delayTime);
+
+  // Type the App you want to open
+  Keyboard.print(app);
+  key(KEY_ENTER, 0);
+  Keyboard.send_now();
+  delay(delayTime);
+}
+
+void key(int KEY, int MODIFIER)
+ {
+  Keyboard.set_modifier(MODIFIER);
+  Keyboard.set_key1(KEY);
+  Keyboard.send_now();
+  delay(20);
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  delay(20);
+ }
+
+void pwn()
+{
+
+  key(KEY_D , MODIFIERKEY_RIGHT_GUI);
+  delay(delayTime);
+
+  openapp("iexplore -k www.youtube.com.br");
+  delay(2000);
+
+}
+
+
+void loop() {
+
+}
